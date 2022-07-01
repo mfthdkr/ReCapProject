7c4dcb25ccba5b534fc91e9cc889d5012309b22f
mfthdkr <50250549+mfthdkr@users.noreply.github.com>
2022-07-01 17:29:53 +0300

    Messages ve Results eklendi.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,10 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string CarAdded = "Araba eklendi.";
+        public static string CarNameInvalid = "Araba ismi geçersiz.";
+        public static string MaintenanceTime = "Sistem bakımda.";
+        public static string CarsListed = "Arabalar listelendi.";
+    }
+}
